70db139b572f5285befd32cbafc8efa18abd8bb3
kandadave <kandachumo@gmail.com>
2025-07-09 21:07:53 +0300

    adds correct jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Just a humble software developer from Nairobi Kenya. One of my biggest passions is creating AI workflows and agents.</p>
+      <img src={ image } alt="I made this" />
+    </div>
+  ) 
 }
 
 export default About;
